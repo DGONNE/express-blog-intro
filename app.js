4d09dc5ed9207f422152516574ff2826597beb98
Davide Gonnelli <davide.gonnelli1@gmail.com>
2025-01-26 23:37:55 +0100

    app.js def

--- app.js
+++ app.js
@@ -15,27 +15,27 @@ app.get("/bacheca", (req, res) => {
 const posts = [
   {
     name: "ciambellone",
-    image: "/images/post1.jpg",
+    image: "http://localhost:3000/images/ciambellone.jpeg",
     taste_scale: "8/10",
   },
   {
     name: "cracker barbabietola",
-    image: "/images/post2.jpg",
+    image: "http://localhost:3000/images/cracker_barbabietola.jpeg",
     tasta_scale: "4/10",
   },
   {
     name: "pane fritto dolce",
-    image: "/images/post3.jpg",
+    image: "http://localhost:3000/images/pane_fritto_dolce.jpeg",
     taste_scale: "8/10",
   },
   {
     name: "pasta barbabietola",
-    image: "/images/post4.jpg",
+    image: "http://localhost:3000/images/pasta_barbabietola.jpeg",
     taste_scale: "5/10",
   },
   {
     name: "torta paesana",
-    image: "/images/post5.jpg",
+    image: "http://localhost:3000/images/torta_paesana.jpeg",
     taste_scale: "7/10",
   },
 ];
